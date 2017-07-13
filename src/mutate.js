22fab6c2c6297f5bf6ce1c25a970f952ac361913
li1 <malte@sandstede.com>
2017-07-14 01:02:46 +0200

    add navigation dispatch action

--- src/mutate.js
+++ src/mutate.js
@@ -10,6 +10,8 @@ import d             from "@clockworks/datascript";
 
 import { Mutations } from "./constants";
 
+import { Navigator } from "./route";
+
 
 const mutate = {};
 
@@ -17,6 +19,17 @@ mutate[Mutations.SUBMIT_STATUS] = (target, text) => {
     console.log("submitting ", text)
     d.transact(target.conn, [[":db/add", 42, ":text", text]])
 }
+
+
+const dispatchNavigation = (db, action) => {
+    const ui = d.vector("db/ident", ":ui")
+    const navigationState = d.get(d.entity(db, ui), "ui/navigationState")
+    const nextState = Navigator.router.getStateForAction(d.toJs(action), d.toJs(navigationState))
+
+    return [[":db/add", ui, "ui/navigationState", nextState]]
+}
+
+mutate[Mutations.NAVIGATION_DISPATCH] = (target: IReconcilable, action) => d.transact(target.conn, [[":db.fn/call", dispatchNavigation, action]])
 
 
 export default mutate;
