57bc75c6fa8a0b7751233b6c4e9813f7944f2b7d
li1 <malte@sandstede.com>
2017-07-13 13:53:46 +0200

    make dates in dummy date with moment

--- src/dummyData.js
+++ src/dummyData.js
@@ -21,10 +21,10 @@ export const dummyData = [
     {
         "task/id": 1,
         "task/snapshot": [{
-            "snapshot/date": {"date/timestamp": time("2017-08-10")},
+            "snapshot/date": {"date/timestamp": moment("2017-08-10").format("x")},
             "snapshot/title": "Nikos Architektur-Analyse",
-            "snapshot/start": {"date/timestamp": time("2017-08-11")},
-            "snapshot/end": {"date/timestamp": time("2017-08-20")},
+            "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
+            "snapshot/end": {"date/timestamp": moment("2017-08-20").format("x")},
             "snapshot/staff": -1
         }],
     },
@@ -32,17 +32,17 @@ export const dummyData = [
         "task/id": 2,
         "task/snapshot": [
             {
-                "snapshot/date": {"date/timestamp": time("2017-08-10")},
+                "snapshot/date": {"date/timestamp": moment("2017-08-10").format("x")},
                 "snapshot/title": "Nikos Sales-Pitch",
-                "snapshot/start": {"date/timestamp": time("2017-08-11")},
-                "snapshot/end": {"date/timestamp": time("2017-08-30")},
+                "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
+                "snapshot/end": {"date/timestamp": moment("2017-08-30").format("x")},
                 "snapshot/staff": -1,
             },
             {
-                "snapshot/date": {"date/timestamp": time("2017-08-22")},
+                "snapshot/date": {"date/timestamp": moment("2017-08-22").format("x")},
                 "snapshot/title": "Jetzt-Maltes Sales-Pitch",
-                "snapshot/start": {"date/timestamp": time("2017-08-11")},
-                "snapshot/end": {"date/timestamp": time("2017-09-05")},
+                "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
+                "snapshot/end": {"date/timestamp": moment("2017-09-05").format("x")},
                 "snapshot/staff": -2,
             }]
     },
