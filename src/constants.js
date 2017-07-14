c504263e5b6908794cdb39bfd6d7977045293260
li1 <malte@sandstede.com>
2017-07-14 16:59:44 +0200

    add further font + color scheme

--- src/constants.js
+++ src/constants.js
@@ -3,15 +3,22 @@
 export const Colors = {
     accent:             "#e91e63",
     background:         "#F8F8F8",
-    headerTint:         "#D73964",
+    headerTint:         "#e91e63",
     headerBackground:   "#FFFFFF",
     accentNeutral:      "#607D8B",
+    disabled:           "#BBB",
+    lowlight:           "#BBB",
+    body:               "#808080",
 }
 
-export const Font = {
-    h1Size:     24,
-    h1Weight:   "600",
-    bodySize:   16,
+export const Fonts = {
+    //scaled with major third
+    h1Size:     27,
+    h1Weight:   "700",
+    h2Size:     21,
+    h2Weight:   "700",
+    h3Size:     17,
+    bodySize:   17,
 }
 
 export const Routes = {
