8cf9c14159735a756c35c9050d8a8855170fe8d5
li1 <malte@sandstede.com>
2017-07-15 20:03:12 +0200

    extend dummy data

--- src/dummyData.js
+++ src/dummyData.js
@@ -23,7 +23,7 @@ export const dummyData = [
     {
         "task/id": 1,
         "task/snapshot": [{
-            "snapshot/date": {"date/timestamp": moment("2017-08-10").format("x")},
+            "snapshot/date": {"date/timestamp": moment("2017-07-10").format("x")},
             "snapshot/title": "Nikos Architektur-Analyse",
             "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
             "snapshot/end": {"date/timestamp": moment("2017-08-20").format("x")},
@@ -32,10 +32,11 @@ export const dummyData = [
         }],
     },
     {
+        ":db/id": -5,
         "task/id": 2,
         "task/snapshot": [
             {
-                "snapshot/date": {"date/timestamp": moment("2017-08-10").format("x")},
+                "snapshot/date": {"date/timestamp": moment("2017-07-10").format("x")},
                 "snapshot/title": "Nikos Sales-Pitch",
                 "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
                 "snapshot/end": {"date/timestamp": moment("2017-08-30").format("x")},
@@ -43,7 +44,7 @@ export const dummyData = [
                 "snapshot/completeness": Completeness.WIP,
             },
             {
-                "snapshot/date": {"date/timestamp": moment("2017-08-22").format("x")},
+                "snapshot/date": {"date/timestamp": moment("2017-07-11").format("x")},
                 "snapshot/title": "Jetzt-Maltes Sales-Pitch",
                 "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
                 "snapshot/end": {"date/timestamp": moment("2017-09-05").format("x")},
@@ -54,7 +55,7 @@ export const dummyData = [
     {
         "task/id": 3,
         "task/snapshot": [{
-            "snapshot/date": {"date/timestamp": moment("2017-08-05").format("x")},
+            "snapshot/date": {"date/timestamp": moment("2017-07-12").format("x")},
             "snapshot/title": "Sales-Umbau",
             "snapshot/start": {"date/timestamp": moment("2017-06-25").format("x")},
             "snapshot/end": {"date/timestamp": moment("2017-10-01").format("x")},
@@ -65,7 +66,7 @@ export const dummyData = [
     {
         "task/id": 4,
         "task/snapshot": [{
-            "snapshot/date": {"date/timestamp": moment("2017-08-05").format("x")},
+            "snapshot/date": {"date/timestamp": moment("2017-07-13").format("x")},
             "snapshot/title": "Stakeholder-Analyse",
             "snapshot/start": {"date/timestamp": moment("2017-07-01").format("x")},
             "snapshot/end": {"date/timestamp": moment("2017-08-01").format("x")},
@@ -73,6 +74,15 @@ export const dummyData = [
             "snapshot/completeness": Completeness.WIP,
         }],
     },
+    // {
+    //         "task/_snapshot": -5,
+    //         "snapshot/date":  {"date/timestamp": moment("2017-09-10").format("x")},
+    //         "snapshot/title": "Jetzt erst recht",
+    //         "snapshot/start": {"date/timestamp": moment("2017-07-01").format("x")},
+    //         "snapshot/end":   {"date/timestamp": moment("2017-08-01").format("x")},
+    //         "snapshot/staff": d.hashMap(d.DB_ID, -2),
+    //         "snapshot/completeness": Completeness.WIP,
+    // },
 ];
 
 export default dummyData
