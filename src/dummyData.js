cf5e924daeceb08bd726b1e88a393096019f027a
li1 <malte@sandstede.com>
2017-07-16 17:56:34 +0200

    extended dummy data for higher level tasks

--- src/dummyData.js
+++ src/dummyData.js
@@ -4,7 +4,8 @@ import d                from "@clockworks/datascript";
 
 import moment       from "moment";
 
-import { Completeness }    from "./constants";
+import { Completeness,
+         RAGs }    from "./constants";
 
 export const dummyData = [
     {
@@ -21,14 +22,19 @@ export const dummyData = [
         "staff/name": "Malte Sandstede",
     },
     {
+        ":db/id": -3,
+        "staff/name": "Christian",
+    },
+    {
         "task/id": 1,
         "task/snapshot": [{
             "snapshot/date": {"date/timestamp": moment("2017-07-10").format("x")},
-            "snapshot/title": "Nikos Architektur-Analyse",
+            "snapshot/title": "Sales-Analyse",
             "snapshot/start": {"date/timestamp": moment("2017-08-11").format("x")},
             "snapshot/end": {"date/timestamp": moment("2017-08-20").format("x")},
             "snapshot/staff": -1,
             "snapshot/completeness": Completeness.DONE,
+            "snapshot/rag": RAGs.GREEN,
         }],
     },
     {
@@ -61,6 +67,9 @@ export const dummyData = [
             "snapshot/end": {"date/timestamp": moment("2017-10-01").format("x")},
             "snapshot/staff": -2,
             "snapshot/completeness": Completeness.WIP,
+            "snapshot/rag": RAGs.GREEN,
+            "snapshot/risk": [{"risk/title": "SAP-Vertragslage schwierig"}],
+            "snapshot/issue":[{"issue/title": "Terminfindungsschwierigkeiten"}],
         }],
     },
     {
@@ -72,6 +81,25 @@ export const dummyData = [
             "snapshot/end": {"date/timestamp": moment("2017-08-01").format("x")},
             "snapshot/staff": -2,
             "snapshot/completeness": Completeness.WIP,
+            "snapshot/rag": RAGs.AMBER,
+            "snapshot/risk": [{ "risk/title": "Stakeholder alle krank"},
+                              { "risk/title": "CEO von Lobby beeinflusst"}],
+            "snapshot/issue":[{"issue/title": "CFO verweigert Kooperation"}],
+        }],
+    },
+    {
+        "task/id": 5,
+        "task/children": [4, 3, 1],
+        "task/snapshot": [{
+            "snapshot/date": {"date/timestamp": moment("2017-07-10").format("x")},
+            "snapshot/title": "Workstream I",
+            "snapshot/start": {"date/timestamp": moment("2017-06-01").format("x")},
+            "snapshot/end": {"date/timestamp": moment("2017-11-01").format("x")},
+            "snapshot/staff": -3,
+            "snapshot/completeness": Completeness.WIP,
+            //These have been reported form lower levels->generelles!
+            "snapshot/risk": [{"risk/title": "3 MA gehen in den Urlaub"}],
+            "snapshot/achievement": [{"achievement/title": "Internes Sales-Team wird mit ausgebildet"}],
         }],
     },
     // {
