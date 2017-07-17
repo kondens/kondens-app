ee52fdb4868c3a0ccd9c1296a2261217b3aa72e7
li1 <malte@sandstede.com>
2017-07-17 21:44:01 +0200

    fix navigation dispatching

--- src/mutate.js
+++ src/mutate.js
@@ -88,12 +88,11 @@ mutate[Mutations.HIDE_EXCLUDED_REPORTABLES] = target => d.transact(target.conn, 
 const dispatchNavigation = (db, action) => {
     const ui = d.vector("db/ident", ":ui")
     const navigationState = d.get(d.entity(db, ui), "ui/navigationState")
-    const nextState = Navigator.router.getStateForAction(d.toJs(action), d.toJs(navigationState))
+    const nextState = Navigator.router.getStateForAction(NavigationActions.navigate(d.toJs(action)), d.toJs(navigationState))
 
     return [[":db/add", ui, "ui/navigationState", nextState]]
 }
 
 mutate[Mutations.NAVIGATION_DISPATCH] = (target: IReconcilable, action) => d.transact(target.conn, [[":db.fn/call", dispatchNavigation, action]])
-
 
 export default mutate;
