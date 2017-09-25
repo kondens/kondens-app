d09ede4a37e3fac7d5a667aab1d306a173828589
li1 <malte@sandstede.com>
2017-09-25 20:27:08 +0200

    integrate sortable list view in own codebase

--- src/views/Report.react.js
+++ src/views/Report.react.js
@@ -32,7 +32,7 @@ import Moment                   from "moment";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import Icon from "react-native-vector-icons/FontAwesome";
-import SortableListView from "react-native-sortable-listview";
+import SortableListView from "../components/SortableListView";
 
 const styles = StyleSheet.create({
     container: {
