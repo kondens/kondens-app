2a685c033abd6e50f35ca46a037146862c641b89
niko <me@nikolasgoebel.com>
2018-05-16 14:34:22 +0200

    Make this thing build on android

--- index.android.js
+++ index.android.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('kondensapp', () => App);
+AppRegistry.registerComponent('kondens-app', () => App);
